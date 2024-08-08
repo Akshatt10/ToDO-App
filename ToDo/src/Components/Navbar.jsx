2fa68f6823617bec1783dabd140a32591093062e
Akshatt10 <akshattyagi1010@gmail.com>
2024-08-09 02:47:10 +0530

    name change

--- ToDo/src/Components/Navbar.jsx
+++ ToDo/src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className='flex justify-around bg-slate-800 text-white'>
         <div className="logo">
-            <span className="font-bold text-xl mx-8">MyTask</span>
+            <span className="font-bold text-xl mx-8">Task Master</span>
         </div>
         <ul className="flex gap-8 mx-9">
         <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
